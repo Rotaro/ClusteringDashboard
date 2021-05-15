5639d218604a1cf51d442c68a560ef282fa4ddaa
Mathias Westenius <rotaro@rotaro.com>
2021-05-15 23:56:27 +0300

    Dimensionality reduction moved to own module.

--- dashboard/app.py
+++ dashboard/app.py
@@ -26,6 +26,7 @@ from dashboard.cache import cache
 from dashboard.data_selection import get_data_source, data_selection_tab
 from dashboard.data_preprocessing import get_preprocessed_data, preprocessing, data_preprocessing_tab
 from dashboard.data_to_array import processing as data_to_array_processing, get_cluster_data, text_to_array_tab
+from dashboard.data_dim_reduction import get_dim_reduction, dim_reductions, dim_reduction_tab
 
 CACHE_CONFIG = {
     "CACHE_TYPE": "simple"
@@ -42,17 +43,6 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 cache.init_app(app.server, config=CACHE_CONFIG)
 
-
-dim_reductions = misc.DropdownWithOptions(
-    header="Choose dimensionality reduction before clustering:", dropdown_id="dim_reduction", dropdown_objects={
-        "None": None,
-        "NMF": model.dim_reduction.NMF,
-        "PCA": model.dim_reduction.PCA,
-        "SVD": model.dim_reduction.SVD,
-        "FactorAnalysis": model.dim_reduction.FactorAnalysis,
-        "TSNE": model.dim_reduction.TSNE,
-    }, include_refresh_button=True
-)
 
 plot_dim_reductions = misc.DropdownWithOptions(
     header="Choose dimensionality reduction method for plotting:", dropdown_id="plot_dim_reduction", dropdown_objects={
@@ -74,17 +64,6 @@ clusterings = misc.DropdownWithOptions(
         "LDA": model.clustering.LDA,
     }, include_refresh_button=True
 )
-
-
-@cache.memoize()
-def get_dim_reduction(data_name, use_sample_perc, selected_columns, selected_preprocessing, chosen_to_array, to_array_options,
-                      dim_reduction, dim_reduction_options):
-    df, data_df = get_cluster_data(data_name, use_sample_perc, selected_columns, selected_preprocessing,
-                                   chosen_to_array, to_array_options)
-    if df is None or data_df is None or not dim_reduction_options:
-        return df, data_df, None
-
-    return df, data_df, pd.DataFrame(dim_reductions.apply(dim_reduction, dim_reduction_options, data_df))
 
 
 @cache.memoize()
@@ -111,15 +90,7 @@ app.layout = html.Div([
             data_selection_tab,
             data_preprocessing_tab,
             text_to_array_tab,
-            dcc.Tab(label="Dimensionality Reduction", children=[
-                html.Div(id="dim_red_area", children=[
-                    # Choose text_to_array method
-                    dim_reductions.generate_dash_element(),
-                    # Display array
-                    html.H5("Dimensionality Reduced Array:", id="dim_red_array_header"),
-                    html.Div(dash_table.DataTable(id="dim_red_array"), id="dim_red_array_div"),
-                ]),
-            ], className="custom-tab", selected_className="custom-tab--selected"),
+            dim_reduction_tab,
             dcc.Tab(label="Hide", children=[], className="custom-tab", selected_className="custom-tab--selected"),
         ]),
     ], style={"border": "grey solid", "padding": "5px"}),
---
+++ dashboard/data_dim_reduction.py
@@ -0,0 +1,48 @@
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+
+import pandas as pd
+
+import model as model
+
+import dashboard.app_misc as misc
+from dashboard.cache import cache
+
+from dashboard.data_to_array import get_cluster_data
+
+
+dim_reductions = misc.DropdownWithOptions(
+    header="Choose dimensionality reduction before clustering:", dropdown_id="dim_reduction", dropdown_objects={
+        "None": None,
+        "NMF": model.dim_reduction.NMF,
+        "PCA": model.dim_reduction.PCA,
+        "SVD": model.dim_reduction.SVD,
+        "FactorAnalysis": model.dim_reduction.FactorAnalysis,
+        "TSNE": model.dim_reduction.TSNE,
+    }, include_refresh_button=True
+)
+
+
+@cache.memoize()
+def get_dim_reduction(data_name, use_sample_perc, selected_columns, selected_preprocessing, chosen_to_array,
+                      to_array_options, dim_reduction, dim_reduction_options):
+    df, data_df = get_cluster_data(data_name, use_sample_perc, selected_columns, selected_preprocessing,
+                                   chosen_to_array, to_array_options)
+    if df is None or data_df is None or not dim_reduction_options:
+        return df, data_df, None
+
+    return df, data_df, pd.DataFrame(dim_reductions.apply(dim_reduction, dim_reduction_options, data_df))
+
+
+dim_reduction_tab = dcc.Tab(
+    label="Dimensionality Reduction", children=[
+        html.Div(id="dim_red_area", children=[
+            # Choose text_to_array method
+            dim_reductions.generate_dash_element(),
+            # Display array
+            html.H5("Dimensionality Reduced Array:", id="dim_red_array_header"),
+            html.Div(dash_table.DataTable(id="dim_red_array"), id="dim_red_array_div"),
+        ]),
+    ], className="custom-tab", selected_className="custom-tab--selected"
+)
